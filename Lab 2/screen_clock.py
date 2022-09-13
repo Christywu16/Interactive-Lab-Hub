c62906ae6849c6db3a21434e2a21a560391aeaaa
jiaojiaojane121 <janewang121@brandeis.edu>
2022-09-12 20:46:44 -0400

    button

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -57,7 +57,8 @@ x1 = 100
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+font = ImageFont.truetype(
+    "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
 font1 = ImageFont.truetype("Roboto-Light.ttf", 45)
 
 # Turn on the backlight
@@ -65,36 +66,37 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-	
-    y1=0
-    #TODO: fill in here. You should be able to look in cli_clock.py and stats.py
+    # TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py
 
-    
+    y1 = 0
+    # TODO: fill in here. You should be able to look in cli_clock.py and stats.py
 
-    draw.text((x1,y1), time.strftime("%a %d" ), font=font, fill="#F4E38E")
+    draw.text((x1, y1), time.strftime("%a %d"), font=font, fill="#F4E38E")
     # draw.text((x2,y2), time.strftime("%H:%M"), font=font, fill="#FFFFF0")
     current_time = time.strftime("%H:%M")
     y = top + 30
-    draw.text((x,y), current_time, font=font1, fill="#FFFFFF")
+    draw.text((x, y), current_time, font=font1, fill="#FFFFFF")
     y += font.getsize(current_time)[1]
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
 
-
     now = datetime.datetime.now()
 
-    if now.hour >= 8 and now.hour<=9:
+    if now.hour >= 8 and now.hour <= 9:
         image = Image.open("1.png")
         backlight = digitalio.DigitalInOut(board.D22)
         backlight.switch_to_output()
         backlight.value = True
-
 
         # Scale the image to the smaller screen dimension
         image_ratio = image.width / image.height
@@ -113,14 +115,13 @@ while True:
         image = image.crop((x, y, x + width, y + height))
         disp.image(image, rotation)
         time.sleep(1)
-    
+
     elif now.hour >= 9:
         image = Image.open("2.png")
         backlight = digitalio.DigitalInOut(board.D22)
         backlight.switch_to_output()
         backlight.value = True
 
-
         # Scale the image to the smaller screen dimension
         image_ratio = image.width / image.height
         screen_ratio = width / height
@@ -138,3 +139,30 @@ while True:
         image = image.crop((x, y, x + width, y + height))
         disp.image(image, rotation)
         time.sleep(1)
+
+    # Button A pressed
+    if buttonB.value and not buttonA.value:  # just button B pressed
+        draw.rectangle((0, 0, width, height), outline="black", fill="black")
+        current_time = time.strftime("%m/%d/%Y  %H:%M")
+        y = top
+        x = int(width/2-font.getsize(current_time)[0]/2)
+        draw.text((x, y), current_time, font=font, fill="#FFFFFF")
+
+    # Button B pressed
+    if buttonA.value and not buttonB.value:
+        draw.rectangle((0, 0, width, height), outline="black", fill="black")
+        hour = int(time.strftime("%H"))
+
+        statement = ""
+        if hour >= 8 and hour < 11:
+            message = "Good morning. Start your day with the first two cups of water"
+        elif hour >= 11 and hour < 14:
+            message = "Stay Hydrated! Drink the 3rd & 4th cups of water"
+        elif hour >= 14 and hour < 17:
+            message = "You are half way through! Keep it going for the 5th & 6th cups of water"
+        elif hour >= 17 and hour < 20:
+            message = "Almost There! Drink your last two cups of to finish your day"
+
+        y = top
+        x = int(width/2-font.getsize(message)[0]/2)
+        draw.text((x, y), message, font=font, fill="#FFFFFF")
